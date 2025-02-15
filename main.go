963b6dfb610d1ec92ca51ae52e313707349b3595
soljesty <hermann42ricefbx@hotmail.com>
2025-02-15 16:21:00 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "time"
+
+    "google.golang.org/grpc"
+    pb "grpc-listener/pb"
+)
+
+func main() {
+    addr := "" // IP to connect to listen for transactions
+    
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+    
+    conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithBlock())
+    if err != nil {
+        log.Fatalf("Connection could not be established: %v", err)
+    }
+    defer conn.Close()
+    
+    client := pb.NewTransactionServiceClient(conn)
+    req := &pb.StreamTransactionsRequest{}
+
+    stream, err := client.StreamTransactions(context.Background(), req)
+    if err != nil {
+        log.Fatalf("Error calling StreamTransactions: %v", err)
+    }
+
+    for {
+        resp, err := stream.Recv()
+        if err != nil {
+            log.Fatalf("Error receiving message: %v", err)
+        }
+        fmt.Printf("Transaction received: %s, Timestamp: %d\n", resp.GetTransactionJson(), resp.GetTimestamp())
+    }
+}
